8796396b7559597e1f17bbca2583e11fed034f95
Kelly Durkin <kdurkin@bearcubs.santarosa.edu>
2024-12-15 12:30:31 -0800

    new appflow ID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  "appId": "cd53b9f6",
-    "appName": "SQL_for_SQuirrels",
+  "appId": "8b45ef38",
+    "appName": "SQL_4_SQuirreLs",
     "webDir": "www",
     "bundledWebRuntime": false
    
